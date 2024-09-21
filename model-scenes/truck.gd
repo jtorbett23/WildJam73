59d9a1a354332ca957e2cb58fddb210be2733755
jtorbett23 <josh.torbett@outlook.com>
2024-09-21 11:03:22 +0100

    big truck & hold pan

---
+++ model-scenes/truck.gd
@@ -0,0 +1,4 @@
+class_name Truck extends Node3D
+
+@onready var collison_shapes = [$Units/StaticBody3D, $Floor/StaticBody3D, $"Window Wall/StaticBody3D",
+$Wall/StaticBody3D, $"Front Panel/StaticBody3D", $"Back Panel/StaticBody3D"]
--- player.gd
+++ player.gd
@@ -3,8 +3,15 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const ROTATION_FACTOR = 0.01
+const RAY_LENGTH = 1000
+
+@onready var ignored_collision_shapes : Array = $"../Truck".collison_shapes + [self]
 @onready var neck : Node3D = $Neck
 @onready var camera : Camera3D = $Neck/Camera3D
+@onready var hands : Node3D = $Hands
+@onready var left_hand: MeshInstance3D = $Hands/Left
+@onready var right_hand: MeshInstance3D = $Hands/Right
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -21,8 +28,44 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * ROTATION_FACTOR)
+			hands.rotate_y(-event.relative.x * ROTATION_FACTOR)
 			camera.rotate_x(-event.relative.y * ROTATION_FACTOR)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+	
+	if event is InputEventMouseButton:
+		if event.button_mask == MOUSE_BUTTON_LEFT:
+			var collider = get_click_collisions()
+			if collider != null:
+				print(collider)
+				if collider.has_node("GrabPoint"):
+					collider.get_parent().remove_child(collider)
+					collider.position = Vector3.ZERO - collider.get_node("GrabPoint").position
+					collider.rotation = Vector3.ZERO 
+					left_hand.add_child(collider)
+		#elif event.button_mask == MOUSE_BUTTON_RIGHT:
+			#var collider = get_click_collisions()
+			#if collider:
+				#if collider.has_node("GrabPoint"):
+					#collider.get_parent().remove_child(collider)
+					#collider.position = Vector3.ZERO - collider.get_node("GrabPoint").position
+					#collider.rotation = Vector3.ZERO 
+					#right_hand.add_child(collider)
+			
+
+func get_click_collisions():
+		var space_state = get_world_3d().direct_space_state
+		var mousepos = get_viewport().get_mouse_position()
+
+		var origin = camera.project_ray_origin(mousepos)
+		var end = origin + camera.project_ray_normal(mousepos) * RAY_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end,4294967295, ignored_collision_shapes)
+		query.collide_with_areas = true
+
+		var result = space_state.intersect_ray(query)
+		if result and result["collider"]:
+			var collider = result["collider"]
+			return collider	
+		return null
 
 
 func _physics_process(delta: float) -> void:
